973bcf43ab81915f55a912050cf60f7a6cae746a
tob2zd <tob2zd@virginia.edu>
2023-02-17 17:41:22 -0500

    Test Commit Office Hours

---
+++ 02_03_sw_skills/testdir/test_shared.py
@@ -0,0 +1,2 @@
+import shared as sh
+sh.afunction()
---
+++ 02_03_sw_skills/testdir/test_upper.py
@@ -0,0 +1,4 @@
+import shared as sh
+
+paragraph = "This was a story written about a man who had a story.\nThe story went on for so                         long that the old writer retired and they\nhad to hire a new one.\n That is to imply, it's a very            long               story.\nvery.\nvery.\nvery long."
+print(sh.string_upper(paragraph))
